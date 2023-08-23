024452ad7a331f5586b705e0329b0c999297613c
gidevan <vsanyc@gmail.com>
2023-08-24 01:02:15 +0300

    KGatling tests are added

---
+++ src/test/java/org/vsanyc/transaction/sandbox/gatling/HttpProducerTest.java
@@ -0,0 +1,31 @@
+package org.vsanyc.transaction.sandbox.gatling;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+
+
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class HttpProducerTest extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080")
+            .header("Content-Type", "application/json")
+            .header("Accept-Encoding", "gzip")
+            .check(status().is(200));
+
+    ScenarioBuilder scn = scenario("Like speaker")
+            .exec(http("like speaker 2").post("/speaker_name2"));
+
+    {
+        setUp(scn.injectOpen(constantUsersPerSec(25000).during(Duration.ofSeconds(1))))
+                .protocols(httpProtocol);
+    }
+
+}
